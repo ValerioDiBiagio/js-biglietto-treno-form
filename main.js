b5bf48159cc3f51b3b9b6ce30b34ce37664e8263
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-03-05 18:18:29 +0100

    feat: add milestone 1

---
+++ main.js
@@ -0,0 +1,58 @@
+const infoForm = document.querySelector('form');
+const inputDistance = document.getElementById('kilometres');
+const inputAge = document.getElementById('myAge');
+
+
+
+infoForm.addEventListener('submit', function(event) {
+    event.preventDefault()
+
+    const kmAmount = parseFloat(inputDistance.value);
+    console.log('Chilometri da percorrere', kmAmount);
+    
+    const userAge = parseInt(inputAge.value);
+    console.log('Età utente', userAge);
+
+    console.log(fullTicketPrice(kmAmount, userAge));
+
+})
+
+
+
+
+function fullTicketPrice (km, age) {
+
+    const standardPrice = 0.21;
+
+    //calcolo del prezzo base
+    let finalPrice = standardPrice * km;
+    //calcolo della percentuale
+    let discountPercentage = 0;
+    
+    if (age < 18) {
+
+    discountPercentage = 20;} 
+    
+    else if (age > 65) {
+
+    discountPercentage = 40;
+    }
+
+    console.log('Percentuale di sconto', discountPercentage);
+    
+    let discount = (finalPrice * discountPercentage) / 100;
+    console.log('Sconto', discount);
+    
+    // calcolare la cifra scontata
+    finalPrice = finalPrice - discount;
+    
+    // ridurre la cifra ottenuta ad un valore con massimo due decimali
+    finalPrice.toFixed(2);
+    console.log( `Il prezzo finale è di ${finalPrice.toFixed(2)} €`);
+
+    return finalPrice;
+} 
+
+
+
+
